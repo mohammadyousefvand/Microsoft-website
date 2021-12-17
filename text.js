68cb66be6b45fff0678901b4a14950d650a2beff
pc <www.mmhmdmm83@gmail.com>
2021-12-17 15:32:05 +0330

    fix menu link

--- text.js
+++ text.js
@@ -1,7 +1,15 @@
 const btn = document.querySelector('.menu')
 const nav = document.querySelector('.nav')
 
+let linkItem = document.querySelectorAll('.link-item')
+
 btn.addEventListener('click', function () {
     nav.classList.toggle('active')
     btn.classList.toggle('rotate')
+})
+
+linkItem.forEach(function (link) {
+    link.addEventListener('click' , function () {
+        nav.classList.remove('active')
+    })
 })
