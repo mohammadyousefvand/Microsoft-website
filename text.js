b7932d03530096d75334ff1de0e21d69838207d8
pc <www.mmhmdmm83@gmail.com>
2021-12-22 21:38:01 +0330

    set scroll top

--- text.js
+++ text.js
@@ -2,6 +2,11 @@ const btn = document.querySelector('.menu')
 const nav = document.querySelector('.nav')
 
 let linkItem = document.querySelectorAll('.link-item')
+let goUpItem = document.querySelector('.go-up')
+
+function goUpHandler () {
+    window.scrollTo(0,0)
+}
 
 btn.addEventListener('click', function () {
     nav.classList.toggle('active')
@@ -13,3 +18,5 @@ linkItem.forEach(function (link) {
         nav.classList.remove('active')
     })
 })
+
+goUpItem.addEventListener('click' , goUpHandler)
